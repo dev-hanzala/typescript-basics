186a2e2ed2261623190cc983baf5d2287720c58e
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-02-28 11:03:21 +0500

    Added step01

---
+++ step01_strong_typing/app.ts
@@ -0,0 +1,15 @@
+//Strong typing:
+let a:string = "Hello!"
+a = "Bye!"
+let b:number = 24
+b = 34
+let c:boolean = true
+c = false
+
+//Inferred typing:
+let d = "happy"
+d = "sad"
+let e = 16.5
+e = 17
+let f = true
+f = false
